eee906eb3c62b0497f33633602a2db91751cf6c5
Adrian Jacobo <jacobo@Adrians-Mac-mini.local>
2025-04-29 22:03:19 -0600

    Se cambia link y se elimina config

--- public_html/partials/strategy.php
+++ public_html/partials/strategy.php
@@ -1,5 +1,5 @@
 <div class="container-fluid text-center strategy-container-banner container-text-center-strategy">
-  <a href="/indexStrategy.html" class="link-duck-hack">
+  <a href="/indexStrategy.php" class="link-duck-hack">
     <h1 class="weight-700 strategy-banner-header">MISIÓN Y VISIÓN</h1>
   </a>
 </div>
