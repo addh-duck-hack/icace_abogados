9437ad68edf0d067275d205ee77265535c34b9ed
Adrian Jacobo <jacobo@Adrians-Mac-mini.local>
2025-05-14 12:28:48 -0600

    Se cambia el video desde youtube

--- public_html/partials/video.php
+++ public_html/partials/video.php
@@ -9,7 +9,7 @@
       </div>-->
       <div class="col-lg-6 col-md-12 col-sm-12">
         <div class="video-container">
-          <iframe src="https://www.youtube.com/embed/hWJCWpLzDxI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/_8NgePmUgS4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       </div>
     </div>
